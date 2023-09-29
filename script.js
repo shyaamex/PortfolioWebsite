06a3e32a32c9157181be889595fea4ddbf38f88f
shyaamex <shyaam.ex@gmail.com>
2023-09-29 14:00:17 +0530

    Commit 006

--- script.js
+++ script.js
@@ -10,6 +10,47 @@ const scroll = new LocomotiveScroll({
 
 
 
+// Cursor animations ( we haven't used mouse trail)
+function circleChaptaKaro() {
+  // define default scale value
+  var xscale = 1;
+  var yscale = 1;
+
+  var xprev = 0;
+  var yprev = 0;
+
+  window.addEventListener("mousemove", function (dets) {
+    clearTimeout(timeout);
+
+    xscale = gsap.utils.clamp(0.8, 1.2, dets.clientX - xprev);
+    yscale = gsap.utils.clamp(0.8, 1.2, dets.clientY - yprev);
+
+    xprev = dets.clientX;
+    yprev = dets.clientY;
+
+    circleMouseFollower(xscale, yscale);
+
+    timeout = setTimeout(function () {
+      document.querySelector(
+        "#minicircle"
+      ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+    }, 100);
+  });
+}
+
+function circleMouseFollower(xscale, yscale) {
+  window.addEventListener("mousemove", function (dets) {
+    document.querySelector(
+      "#minicircle"
+    ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
+  });
+}
+
+circleChaptaKaro();
+circleMouseFollower();
+
+
+
 
 
 
@@ -79,7 +120,7 @@ firstPageAnim();
 
 
 
-document.querySelectorAll("#elem2").forEach(function (elem) {
+document.querySelectorAll(".elem").forEach(function (elem) {
     var rotate = 0;
     var diffrot = 0;
   
